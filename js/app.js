1f79c298a1bbf30ebd01a904e5e5f4c0110be11b
jamal <ekonokevin@gmail.com>
2019-03-09 11:35:36 +0100

    tout les module ok avec angular

--- js/app.js
+++ js/app.js
@@ -1,53 +1,21 @@
-app.controller('mes_compentences', function($scope) {
-    $scope.nom1 = "compentence1";
-    $scope.indice1 = "98";
+app.controller('About', function($scope) {
+    $scope.titre_section = "A PROPOS";
 
-    $scope.nom2 = "compentence2";
-    $scope.indice2 = "75";
+    $scope.nom = "Kevin Obam Ekono";
+    $scope.fonction = "FULL+ STACK+ DEVELOPPEUR+ WEB+ MOBILE+ STAND ALONE+";
 
-    $scope.nom3 = "compentence3";
-    $scope.indice3 = "80";
 
-    $scope.nom4 = "compentence4";
-    $scope.indice4 = "80";
+    $scope.descriptif = "Bonjour, je suis Kevin Obam Ekono. J'ai 2 ans d'expérience dans le developpement Web et UX. Je travaille sur une variété de technologies, front-end et back-end, je suis tres sociable et j'adore les nouveaux defis car pour moi un projet c'est un defis ainsi qu'une opportunité d'apprendre des nouvelles choses.j'ai une mobilité national ou international.j'aime le travail bien fait et j'aime beaucoup travailler en equipe. Pour moi <<la programmation c'est l'école de la rue >>";
 
 
 
-});
-app.controller('connaissances1', function($scope) {
-    $scope.connaissances1 = "connaissance1";
-    $scope.connaissances2 = "connaissance2";
-    $scope.connaissances3 = "connaissance3";
-    $scope.connaissances4 = "connaissance4";
-    $scope.connaissances5 = "connaissance5";
-    $scope.connaissances6 = "connaissance6";
-    $scope.connaissances7 = "connaissance7";
+    $scope.image = "images/jamalentier2.png";
+
+    $scope.date_naissance = "08/03/1995";
+    $scope.phone = " (+237) 691 10 01 28 /693 44 18 40";
+    $scope.email = "ekonokevin@gmail.com ";
+    $scope.adresse = "Cameroun, Yaoundé Bonamoussadi rue 4025";
+    $scope.websitef = "kevinobam.objis.com";
 
 
-});
-
-app.controller('connaissances2', function($scope) {
-    $scope.connaissances1 = "connaissance1";
-    $scope.connaissances2 = "connaissance2";
-    $scope.connaissances3 = "connaissance3";
-    $scope.connaissances4 = "connaissance4";
-    $scope.connaissances5 = "connaissance5";
-    $scope.connaissances6 = "connaissance6";
-    $scope.connaissances7 = "connaissance7";
-
-
-});
-
-
-
-app.controller('language', function($scope) {
-    $scope.nom1 = "Fr";
-    $scope.indice1 = "75";
-    $scope.niveau1 = "ADVANCE";
-    $scope.nom2 = "compentence2";
-    $scope.indice2 = "98";
-    $scope.niveau2 = "ADVANCE";
-    $scope.nom3 = "compentence3";
-    $scope.indice3 = "50";
-    $scope.niveau3 = "MOYEN";
 });
